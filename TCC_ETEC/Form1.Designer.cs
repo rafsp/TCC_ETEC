c2ef78d38f46245576003b9218ced96a9c8e553e
RAFAEL PEREIRA <rafael.pereira236@etec.sp.gov.br>
2023-10-25 21:59:45 -0300

    Alteração de layout

--- TCC_ETEC/Form1.Designer.cs
+++ TCC_ETEC/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(348, 107);
+            this.button1.Location = new System.Drawing.Point(505, 172);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "teste";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
